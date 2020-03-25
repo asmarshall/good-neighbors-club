09e3396d63930ea7494e015d49f3019076b285fa
A. MARSHALL <HELLO@ANGELASMARSHALL.com>
2020-03-24 23:59:11 -0700

    Initial routing and templating of home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  get 'home/index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'post/create'
+  post 'post/store'
+
+  root 'home#index'
 end
