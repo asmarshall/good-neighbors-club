440dec59f957d77450442b18d8a7be4198de31fd
A. MARSHALL <HELLO@ANGELASMARSHALL.com>
2020-04-04 23:49:20 -0700

    Prettier viewable categories

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,20 @@
 class Post < ApplicationRecord
   CATEGORIES = %w[restaurant bar brewery coffee tea boutique farm flowers fitness resources mental_health general_ways_to_help other].freeze
+  VIEWABLE_CATEGORIES = [
+    ['Restaurant', 'restaurant'],
+    ['Bar', 'bar'],
+    ['Brewery', 'brewery'],
+    ['Coffee', 'coffee'],
+    ['Tea', 'tea'],
+    ['Boutique', 'boutique'],
+    ['Farm', 'farm'],
+    ['Flowers', 'flowers'],
+    ['Fitness', 'fitness'],
+    ['Resources', 'resources'],
+    ['Mental Health', 'mental_health'],
+    ['General ways to help', 'general_ways_to_help'],
+    ['Other', 'other']
+  ]
 
   scope :restaurants, -> { where(category: 'restaurant') }
   scope :bars_breweries, -> { where(category: %w[bar brewery]) }
