06417fa381d6c94514136e743147cc63c8448529
A. MARSHALL <HELLO@ANGELASMARSHALL.com>
2020-04-08 20:45:36 -0700

    Styling nit-picks

--- app/models/post.rb
+++ app/models/post.rb
@@ -20,7 +20,7 @@ class Post < ApplicationRecord
   scope :bars_breweries, -> { where(category: %w[bar brewery]) }
   scope :coffee_tea_shops, -> { where(category: %w[coffee tea]) }
   scope :boutiques, -> { where(category: 'boutique') }
-  scope :farms_flowers, -> { where(category: %w[farm flower]) }
+  scope :farms_flowers, -> { where(category: %w[farm flowers]) }
   scope :fitness, -> { where(category: %w[fitness mental_health]) }
   scope :general_ways_to_help, -> { where(category: 'general_ways_to_help') }
   scope :resources_mental_health, -> { where(category: 'resources') }
