5d9beb002907c02bc587781ceb93b5305a738362
A. MARSHALL <HELLO@ANGELASMARSHALL.com>
2020-04-12 13:40:29 -0700

    Order by created at ASC

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -12,34 +12,34 @@ class PagesController < ApplicationController
   end
 
   def restaurants
-    @restaurants = Post.is_public.restaurants.order("created_at DESC")
+    @restaurants = Post.is_public.restaurants.order("created_at")
   end
 
   def bars_breweries
-    @bars_breweries = Post.is_public.bars_breweries.order("created_at DESC")
+    @bars_breweries = Post.is_public.bars_breweries.order("created_at")
   end
 
   def coffee_tea_shops
-    @coffee_tea_shops = Post.is_public.coffee_tea_shops.order("created_at DESC")
+    @coffee_tea_shops = Post.is_public.coffee_tea_shops.order("created_at")
   end
 
   def boutiques
-    @boutiques = Post.is_public.boutiques.order("created_at DESC")
+    @boutiques = Post.is_public.boutiques.order("created_at")
   end
 
   def farms_flowers
-    @farms_flowers = Post.is_public.farms_flowers.order("created_at DESC")
+    @farms_flowers = Post.is_public.farms_flowers.order("created_at")
   end
 
   def fitness
-    @fitness = Post.is_public.fitness.order("created_at DESC")
+    @fitness = Post.is_public.fitness.order("created_at")
   end
 
   def general_ways_to_help
-    @general_ways_to_help = Post.is_public.general_ways_to_help.order("created_at DESC")
+    @general_ways_to_help = Post.is_public.general_ways_to_help.order("created_at")
   end
 
   def resources_mental_health
-    @resources_mental_health = Post.is_public.resources_mental_health.order("created_at DESC")
+    @resources_mental_health = Post.is_public.resources_mental_health.order("created_at")
   end
 end
